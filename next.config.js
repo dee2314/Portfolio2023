1143b78b2a842af01a78900535bb2808a31197dc
Dee <twitchg650@gmail.com>
2023-10-09 18:58:03 -0500

    modify next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   // Add the following line to specify the output directory
-  distDir: 'out',
+  distDir: ".next", //your publish directory is '.next'
 }
 
 module.exports = nextConfig;
