c1cb14e0e0d2495415c367a46da7b1f68f82f463
Dee <twitchg650@gmail.com>
2023-10-09 19:41:29 -0500

    some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/CV-Dee.pdf"
+                  href="/CV-Dee Richards.pdf"
                   target="_blank"
                   className="flex items-center bg-dark text-light p-2.5 px-6
             rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
