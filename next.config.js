50b4e6d570e26f721a1f079ed4f153304554c739
Deija <m>
2026-03-16 13:09:13 -0500

    Fix Site

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Add the following line to specify the output directory
-  distDir: ".next", //your publish directory is '.next'
-}
+  output: "export",
+};
 
 module.exports = nextConfig;
